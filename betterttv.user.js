438a481429721435a5dabffa0395d174bc13a734
Vitalij Ryndin <sys@crashmax.ru>
2021-11-28 21:28:16 +0800

    update hostname path

--- betterttv.user.js
+++ betterttv.user.js
@@ -3,7 +3,7 @@
 // @namespace       BTTV
 // @description     Enhances Twitch with new features, emotes, and more.
 // @copyright       NightDev, LLC
-// @icon            https://bttv.vercel.app/assets/logos/bttv_logo.png
+// @icon            https://crashmax-dev.github.io/bttv-old/assets/logos/bttv_logo.png
 // @grant           none
 // @match           https://*.twitch.tv/*
 // @version         7.4.2
@@ -12,7 +12,7 @@
 (function betterttv() {
   var script = document.createElement('script')
   script.type = 'text/javascript'
-  script.src = 'https://bttv.vercel.app/betterttv.js'
+  script.src = 'https://crashmax-dev.github.io/bttv-old/betterttv.js'
   var head = document.getElementsByTagName('head')[0]
   if (!head) return
   head.appendChild(script)
